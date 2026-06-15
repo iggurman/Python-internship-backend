08472883b326e1f90e66d7ccad75d24c62e1e979
Gurman <garggurman@gmail.com>
2026-06-15 17:21:08 +0530

    final committs to the project and also created just a file of week 2 project

---
+++ month 1/week2/day1/class_list.py
@@ -0,0 +1,44 @@
+# List can store same or different data types
+# List is mutable (can be changed)
+
+my_list = [12, "demo", 3.6, True]
+
+my_list.insert(1, 16)      # Insert 16 at index 1
+my_list.append(98)         # Add single value at end
+
+list2 = [1, "hello", "yo", False]
+my_list.extend(list2)      # Add multiple values
+
+my_list.remove(True)       # Remove first occurrence of True
+
+my_list.pop(3)             # Remove value at index 3
+                           # If no index is given, removes last element
+
+print(my_list.index("demo"))   # Find index of value
+print(my_list.count(12))       # Count occurrences
+
+# my_list.sort()               # Works only if elements are comparable
+
+print(my_list)
+
+my_list.clear()                # Empty the list
+# Tuple is immutable (cannot be changed)
+
+my_tuple = ("demo", 12, 55)
+
+# Accessing values
+print(my_tuple[0])
+print(my_tuple[1])
+print(my_tuple[2])
+
+# Packing means storing multiple values in one tuple.
+student = ("demo", 12, 55)
+
+# Unpacking
+student = ("demo", 12, 55)
+
+name, age, weight = student
+
+print(name)
+print(age)
+print(weight)
---
+++ month 1/week2/day1/class_string.py
@@ -0,0 +1,79 @@
+# #strings
+# multi line strings are called doc strings
+
+str1='hello  ' #space also counts as a character
+str2="hello"
+str3="""hello
+        hello i am gurman
+        whatsapp"""
+print(str1,str2,str3)
+
+#to print id 
+print(id(str1))   
+
+#to print indexing               
+print(str1[1])  #indexing is allowed 
+
+#assigning values to a new string cause string is immutable
+new_str= "m"+str1[1:]
+print(new_str)
+
+
+# its ordered, immutable (once declared it cant be changed )
+
+for i in range(len(str1)-1):
+    print(str1[i])
+
+print(str1[-3])
+#will print e from helo
+# indexing is from 0 to n-1
+# negative indexing is start from last at -1
+
+
+#string Slicing used to extract a part of the sequence of a string
+str4="gurman garg" #space will also be counted and is like a range funcṭion
+print(str4[0:8:2]) #start:stop:step (colon is important)
+
+#string concat
+first="gurman"
+last="garg"
+str1=first+" "+last #with use of 3rd variable
+print(str1)
+print(first+" "+last) #without 3rd variable
+print("yo"*5) #multiplication of strings 5 times
+
+# string and python both are case sensitive
+
+str5="demo" #both have diff ids but we can create a new string instead of modifying the old one
+str5=str5+"demo2"
+print(str5)
+
+#check if char is in the string or not
+print("m" in str5) # True
+
+# lower
+print(str5.lower())
+#capitalize 1st
+print(str5.capitalize())
+#upper case
+print(str5.upper())
+#strip used to strip the spaces of the string
+print(str1.strip)
+#replace method used to replace the word in a string
+print(str5.replace("demo","python"))
+#split used to split words on basis of space(or anything we specify) and adds them to a list
+text="python c c++ java"
+print(text.split(" "))
+#join used to join the strings in a list to convert back to a string
+list=["banana","apple","mango"]
+print(" ".join(list))
+
+text="this is a python"
+print(text.count("a","e"))
+
+
+count=0
+for char in text:
+    if char in "aeiou":
+        count+=1
+print("count")
