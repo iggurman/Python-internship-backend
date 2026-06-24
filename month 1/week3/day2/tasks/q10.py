236c9a7e1e50d36bbf0b0fb2f7d93003c0d75b54
Gurman <garggurman@gmail.com>
2026-06-24 15:02:25 +0530

    done practical tasks

--- month 1/week3/day2/tasks/q10.py
+++ month 1/week3/day2/tasks/q10.py
@@ -1,12 +1,14 @@
 class Counter():
     def __init__(self,):
-        self.current=0
+        self.current=97
         
     def __iter__(self):
         return self
     def __next__(self):
-        value=self.current
+        value=chr(self.current)
         self.current+=1
+        if self.current>122:
+            self.current=97
         return value
     
 obj = Counter()
